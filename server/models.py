e699345f6cbd4b1885336e6ce04743f4cb40d9c9
Michael Gilman <michaelgilman@Michaels-MacBook-Pro.local>
2020-07-01 18:53:38 -0700

    I think I finished the solow model skeleton. it at least generates a list of points along the capital curve.

--- server/models.py
+++ server/models.py
@@ -2,12 +2,23 @@
 # to play around with the variables to get the model on different steady states. we will see. :)
 
 # pretty sure I'm doing this wrong. Need to reference my notes on Recursive dynamic programming.
-def solow(K_0,L_0,a,b,d,g,s):
-    Y = a * (K_0 ^ b) * (L_0 ^ (1 - b))
-    K_1 = (1 + s - d) * K_0
-    L_1 = (1 + g) * L_0
 
-    return
+
+def solow(K_0, L_0, a, b, d, g, s, duration):
+    k_0 = K_0
+    l_0 = L_0
+    y_0 = a * (k_0 ** b) * (l_0 ** (1 - b))
+    points = list()
+
+    for i in range(1, duration):
+        k_1 = (1 + s - d) * y_0
+        l_1 = (1 + g) * l_0
+        y_1 = a * (k_1 ** b) * (l_1 ** (1 - b))
+        points.append([k_1, k_0])
+        k_0 = k_1
+        l_0 = l_1
+        y_0 = y_1
+    return points
 
 
 if __name__ == "__main__":
