ee0ae29ee8746607c1f2a21731e5df2a935de625
Michael Gilman <michaelgilman@Michaels-MacBook-Pro.local>
2020-07-03 18:35:46 -0700

    Added default values for solow model!

--- server/models.py
+++ server/models.py
@@ -4,22 +4,25 @@
 # pretty sure I'm doing this wrong. Need to reference my notes on Recursive dynamic programming.
 
 
-def solow(K_0, L_0, a, b, d, g, s, duration):
+def solow(K_0 = 1, L_0 = 1, A_0 = 1, alpha = 0.5, d = 0.05, g = 0.03, n = 0.02, s = 0.1, duration = 100):
     k_0 = K_0
     l_0 = L_0
-    y_0 = a * (k_0 ** b) * (l_0 ** (1 - b))
+    a_0 = A_0
+    y_0 = (k_0 ** alpha) * ((A_0 * l_0) ** (1 - alpha))
     points = list()
 
     for i in range(1, duration):
         k_1 = (1 + s - d) * y_0
         l_1 = (1 + g) * l_0
-        y_1 = a * (k_1 ** b) * (l_1 ** (1 - b))
+        a_1 = (1 +n) * a_0
+        y_1 = (k_1 ** alpha) * ((a_1 * l_1) ** (1 - alpha))
         points.append([k_1, k_0])
         k_0 = k_1
         l_0 = l_1
         y_0 = y_1
+        a_0 = a_1
     return points
 
 
 if __name__ == "__main__":
-    solow(1,1,1,.5,.1,.05,.15,10)
+    solow()
--- server/plots.py
+++ server/plots.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.append(os.getcwd)
+sys.path.append(os.getcwd() + "/server")
 from models import solow
 
-points = models.solow()
+points = solow()
+
