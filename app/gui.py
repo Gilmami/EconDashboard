7f28c2a73d7c3adc5b81f54fe9ab946a434e077e
Michael Gilman <michael.gilman@outlook.com>
2020-07-14 18:35:20 -0700

    Attempted to make graph change with slider values. Failed. Added a_0 to my slider list. Success.

--- app/gui.py
+++ app/gui.py
@@ -26,36 +26,46 @@ l0_slide = Scale(master = root, from_= 1000, to = 0, label = "L0".translate(subs
 l0_slide.set(1)
 l0_slide.grid(column=1, row=1)
 
+a0_slide = Scale(master = root, from_= 1, to = 0, resolution = .01, label = "a")
+a0_slide.set(.7)
+a0_slide.grid(column=3, row=1)
+
 alpha_slide = Scale(master = root, from_= 1, to = 0, resolution = .01, label = "a")
 alpha_slide.set(.7)
-alpha_slide.grid(column=3, row=1)
+alpha_slide.grid(column=4, row=1)
 
 delta_slide = Scale(master = root, from_= 1, to = 0, resolution = .01, label = "d")
 delta_slide.set(.05)
-delta_slide.grid(column=4, row=1)
+delta_slide.grid(column=5, row=1)
 
 g_slide = Scale(master = root, from_= 1, to = 0, resolution = .01, label = "g")
 g_slide.set(.03)
-g_slide.grid(column=5, row=1)
+g_slide.grid(column=6, row=1)
 
 n_slide = Scale(master = root, from_= 1, to = 0, resolution = .01, label = "n")
 n_slide.set(.02)
-n_slide.grid(column=6, row=1)
+n_slide.grid(column=7, row=1)
 
 s_slide = Scale(master = root, from_= 1, to = 0, resolution = .01, label = "s")
 s_slide.set(.1)
-s_slide.grid(column=7, row=1)
+s_slide.grid(column=8, row=1)
 
-# initialize and create the graph for the solow model with parameter values set by sliders
+# initialize and create the graph for the model with parameter values (ideally) set by sliders
 figure = plt.Figure(figsize=(6,5), dpi=100)
 ax = figure.add_subplot(1, 1, 1)
 
 canvas = FigureCanvasTkAgg(figure, root)
 canvas.get_tk_widget().grid(row=0, column=0, columnspan=8)
 
-kprime = np.diff(models.solow().k0)
+# i'm not sure this should be here like this, but IDK what to do about updating through
+# dang sliders. I'll do more reading and try to figure it out later.
 
-ax.plot(models.solow().k0, models.solow().k1, color="#C41E3A")
+graph = models.solow(k_0=k0_slide.get(), l_0=l0_slide.get(), a_0=a0_slide.get(),
+                     alpha=alpha_slide.get(), d=delta_slide.get(), g=g_slide.get(),
+                     n=n_slide.get(), s=s_slide.get())
+
+ax.plot(graph.k0, graph.k1, color="#C41E3A")
+
 
 root.mainloop()
 
